3affe2eadadf8635f895bbe336de8cbc9d402d3b
lishuai <lishuai@oak-inc.com>
2018-02-09 14:05:21 +0800

    KMP算法的下标问题： 课本上例子0号单元存字符串长度，以pascal语言为例，c语言实现时无法实现。应以kmp_2.c为准

--- program/4-kmp.c
+++ program/4-kmp.c
@@ -1,37 +1,58 @@
 #include "common.h"
+#include <string.h>
 
 #ifndef MAXSTRLEN
 #define MAXSTRLEN 255
 #endif
 
-typedef unsigned char SString[MAXSTRLEN+1];//0号单元存放串的长度
+typedef char SString[MAXSTRLEN+1];//0号单元存放串的长度（pascal语言），c语言不是这样的
 
 //函数原型声明
 int Index_KMP(SString S,SString T,int pos);
 void get_next(SString T,int next[]);
 void get_nextval(SString T,int next[]);
+void format_SSting(SString T);
 
-int next[5];
+int next[6];
 
 int main(int argc, char const *argv[])
 {
     SString S = "ababcabcacbab";
+    format_SSting(S);
+    
     SString T = "abcac";
-    get_nextval(T,next);
-    // printf("%d\n",next[0]);
-    // printf("%d\n",next[1]);
-    // printf("%d\n",next[2]);
-    // printf("%d\n",next[3]);
-    // printf("%d\n",next[4]);
+    format_SSting(T);
+    
+    // return 0;
+    get_next(T,next);
+    printf("%d\n",next[0]);
+    printf("%d\n",next[1]);
+    printf("%d\n",next[2]);
+    printf("%d\n",next[3]);
+    printf("%d\n",next[4]);
+    printf("%d\n",next[5]);
     int pos = Index_KMP(S,T,1);
     printf("%d\n", pos);
     return 0;
 } 
+
+//为了维护和书本的下标一致，所有元素往右移动一个，0号单元不用
+void format_SSting(SString S) {
+    int s_len = strlen(S) - 1;
+    for (int i = s_len-1; i >= 1; --i)
+    {
+        S[i+1] = S[i];
+    }
+    S[0] = '^';//第一个单元不用了，废弃
+}
+
 //利用模式串T的next函数，求T在主串S中第pos个字符之后的位置的KMP算法
 int Index_KMP(SString S,SString T,int pos)
 {
     int i = pos,j = 1;
-    while(i <= S[0] && j <= T[0]) {
+    int s_len = strlen(S) - 1;
+    int t_len = strlen(T) - 1;
+    while(i <= s_len && j <= t_len) {
         if (j == 0 || S[i] == T[j])
         {
             ++i;
@@ -40,9 +61,9 @@ int Index_KMP(SString S,SString T,int pos)
             j = next[j];
         }
     }
-    if (j > T[0])
+    if (j > t_len)
     {
-        return i-T[0];
+        return i - t_len;
     } else {
         return 0;
     }
@@ -54,7 +75,8 @@ void get_next(SString T,int next[])
     int i = 1;
     next[1] = 0;
     int j = 0;
-    while(i < T[0]) {
+    int t_len = strlen(T) - 1;
+    while(i < t_len) {
         if (j == 0 || T[i] == T[j])
         {
             ++i;
@@ -72,7 +94,8 @@ void get_nextval(SString T,int next[])
     int i = 1;
     next[1] = 0;
     int j = 0;
-    while(i < T[0]) {
+    int t_len = strlen(T) - 1;
+    while(i < t_len) {
         if (j == 0 || T[i] == T[j])
         {
             ++i;
--- program/4-kmp_2.c
+++ program/4-kmp_2.c
@@ -1,41 +1,55 @@
-#include "common.h"
+/*
+    KMP算法的c语言实现
+ */
+#include <stdio.h>
 #include <string.h>
+
+#ifndef MAXSTRLEN
+#define MAXSTRLEN 100
+#endif
 
 typedef struct
 {
-    char string[100];
-    int length;
+    char string[MAXSTRLEN];//串（定长顺序存储0-...,下标从0开始，区别于《数据结构》课本)
+    int length;//串长度
 } SString;
 
 void getnext(SString T,int next[]){
     int i,j;
-    next[0]=-1;//next[0]放上-1   
-    i=0;//指向字符串每个字符的指针   
-    j=-1;
-    while(i<T.length){//没有到达结尾的话   
-        if(j==-1||T.string[i]==T.string[j]){//如果是第一个字符或遇到相同的字符   
-            i++;j++;next[i]=j;
-        } 
-        else  
-            j=next[j];
+    next[0] = -1;//next[0]放上-1   
+    i = 0;//指向字符串每个字符的指针   
+    j = -1;
+    while(i < T.length){//没有到达结尾的话   
+        if(j == -1 || T.string[i] == T.string[j]){//如果是第一个字符或遇到相同的字符   
+            i++;
+            j++;
+            next[i] = j;
+        } else {
+            j = next[j];
+        }
     }
-    for(i=0;i<T.length;i++){//输出next[]值   
+    for(i = 0;i < T.length;i++){//输出next[]值   
         printf("%d ",next[i]);
     }
 } 
 
 int kmp(SString S,SString T,int next[]){
     int i,j;
-    i=j=0;
-    while(i<S.length&&j<T.length){
-        if(j==-1||S.string[i]==T.string[j]){
-            i++;j++;
+    i = j = 0;
+    while(i < S.length && j < T.length){
+        if(j == -1 || S.string[i] == T.string[j]){
+            i++;
+            j++;
         }
-        else  
-            j=next[j];
+        else {
+            j = next[j];
+        }
     }
-    if(j==T.length) return i-T.length;
-    else return -1;
+    if(j == T.length) {
+        return i - T.length;
+    } else {
+        return -1;
+    }
 }
 
 int main(){
